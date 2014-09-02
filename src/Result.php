33ba42d9797f4304b2fa4e3dbc26413702759e66
Jonas Felix <jonas.felix@icloud.com>
2014-09-02 11:31:04 +0200

    renames getAssoc to getFullResponse, implements extractResult, first minimal error handling (now first exceptions are thrown), separationg of cursor, result and response data

--- src/Result.php
+++ src/Result.php
@@ -37,22 +37,54 @@ class Result {
 	public $search;
 	
 	/**
+	 * @var array
+	 */
+	private $response;
+	
+	/**
+	 * @var array
+	 */
+	private $cursor;
+	
+	/**
+	 * @var array
+	 */
+	private $result;
+	
+	/**
 	 * 
 	 * @param \Httpful\Response $httpfulResponse
 	 * @param \felixideas\GoogleSearchWrapper\Search $search
 	 */
-	public function __construct($httpfulResponse, $search) {
-		$this->search = $search;
-		$this->httpfulResponse = $httpfulResponse;
+	public function __construct($search) {
+		$this->search = clone $search;
+		$this->extractResult();
 	}
 	
+	/**
+	 * 
+	 */
+	private function extractResult() {
+		$this->response = json_decode($this->search->httpfulResponse->body, true);
+
+		if($this->search->httpfulResponse->hasErrors() || $this->response['responseStatus'] >= 400) {
+			if(!empty($this->response['responseDetails'])) {
+				throw new \Exception($this->response['responseDetails'], $this->search->httpfulResponse->code);
+			} else {
+				throw new \Exception('No response details by google. HTTP Code: ' . $this->search->httpfulResponse->code, $this->search->httpfulResponse->code);
+			}
+		}
+		
+		$this->results = $this->response['responseData']['Result'];
+		$this->cursor = $this->response['responseData']['Cursor'];
+	}
 
 	/**
 	 * return the result as associative array
 	 * 
 	 * @return array search result
 	 */
-	public function getAssoc() {
-		return json_decode($this->httpfulResponse->body, true);
+	public function getFullResponse() {
+		return $this->response;
 	}
 }

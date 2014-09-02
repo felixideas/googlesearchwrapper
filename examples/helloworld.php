2417bf4db4b7a4e622366e133a90ff28a5c11136
Jonas Felix <jonas.felix@icloud.com>
2014-09-02 11:31:29 +0200

    adapted new function name getFullResponse()

--- examples/helloworld.php
+++ examples/helloworld.php
@@ -20,6 +20,6 @@ require('../vendor/autoload.php');
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-$helloWorldSearch = \felixideas\GoogleSearchWrapper\Search::search('Hello World')->run()->getAssoc();
+$helloWorldSearch = \felixideas\GoogleSearchWrapper\Search::search('Hello World')->run()->getFullResponse();
 
 print_r($helloWorldSearch);
